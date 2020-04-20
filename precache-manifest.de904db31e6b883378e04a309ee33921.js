f53d721462cb6cf5451f169b27da1905eab36050
mahdislimane <slimane_mahdi@yahoo.fr>
2020-04-20 21:57:34 +0100

    Updates

---
+++ precache-manifest.de904db31e6b883378e04a309ee33921.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d5048c48e61b35f68b53ca1e716e97",
+    "url": "/routercheck/index.html"
+  },
+  {
+    "revision": "1a7503fc5056dd8c1088",
+    "url": "/routercheck/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e64aee02b06015f59bc1",
+    "url": "/routercheck/static/css/main.5c69579d.chunk.css"
+  },
+  {
+    "revision": "1a7503fc5056dd8c1088",
+    "url": "/routercheck/static/js/2.74564413.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/routercheck/static/js/2.74564413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64aee02b06015f59bc1",
+    "url": "/routercheck/static/js/main.02095d95.chunk.js"
+  },
+  {
+    "revision": "d526a9d61c49c74eeb53",
+    "url": "/routercheck/static/js/runtime-main.c1ca866e.js"
+  }
+]);
